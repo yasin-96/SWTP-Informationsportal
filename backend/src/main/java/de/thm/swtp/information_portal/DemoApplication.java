aa27c25639f8b5cb8d61f2a987904527d5fe6173
Alexander Jajzler <jaijszler@gmail.com>
2020-05-03 18:19:40 +0200

    Initial Commit

---
+++ backend/src/main/java/de/thm/swtp/information_portal/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.thm.swtp.information_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/thm/swtp/information_portal/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thm.swtp.information_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
