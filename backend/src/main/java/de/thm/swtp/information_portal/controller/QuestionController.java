702a20881391f049a72318a6d6029fb9783c37b6
yers32 <yasin.eraslan@mni.thm.de>
2020-05-07 22:13:01 +0200

    Service und Controller für Question geschrieben

---
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/QuestionController.java
@@ -0,0 +1,50 @@
+package de.thm.swtp.information_portal.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.thm.swtp.information_portal.models.Question;
+import de.thm.swtp.information_portal.service.QuestionService;
+
+@RestController
+@RequestMapping("/api")
+public class QuestionController {
+	
+	@Autowired
+	private QuestionService questionSerice;
+	
+	@GetMapping("/questions")
+	public List<Question> getAllQuestions() {
+		return questionSerice.getAllQuestions();
+	}
+	
+	@GetMapping("/question/{id}")
+	public ResponseEntity<Question> getQuestion(@PathVariable String id){
+		Optional<Question> question = questionSerice.getQuestion(id);
+		ResponseEntity<Question> quest = question.map(response->ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+		return quest;
+		
+	}
+	
+	
+	ResponseEntity<Question> postQuestion(@Valid @RequestBody Question questionBody) throws URISyntaxException{
+	Question question = questionSerice.postQuestion(questionBody);
+	return ResponseEntity.created(new URI("/api/question" + question.getId())).body(question);
+	
+	
+}
+}
---
+++ backend/src/main/java/de/thm/swtp/information_portal/service/QuestionService.java
@@ -0,0 +1,41 @@
+package de.thm.swtp.information_portal.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import de.thm.swtp.information_portal.models.Question;
+import de.thm.swtp.information_portal.repositories.QuestionRepository;
+
+@Service
+public class QuestionService {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	//private List<Question> questions = new ArrayList<Question>();
+	
+
+	public List<Question> getAllQuestions(){
+		return questionRepository.findAll();
+	}
+	
+	
+	
+	public Question postQuestion(@RequestBody Question question) {
+		return questionRepository.save(question);
+	}
+	
+	public Optional<Question> getQuestion(String id) {
+		Optional<Question> question = questionRepository.findById(id);
+		return question;
+	}
+}
