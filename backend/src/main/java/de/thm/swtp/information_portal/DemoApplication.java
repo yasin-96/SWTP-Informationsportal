2c2add9c9eafcb558b943b1eda5098ae5da93810
yers32 <yasin.eraslan@mni.thm.de>
2020-05-07 23:50:03 +0200

    Main Application aufgeräumt

--- backend/src/main/java/de/thm/swtp/information_portal/DemoApplication.java
+++ backend/src/main/java/de/thm/swtp/information_portal/DemoApplication.java
@@ -13,35 +13,10 @@ import de.thm.swtp.information_portal.repositories.QuestionRepository;
 
 @SpringBootApplication
 public class DemoApplication  {
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
 
-@Component
-class Runner implements CommandLineRunner {
 
-private final QuestionRepository questionRepository;
-	
-	public Runner(QuestionRepository questionRepository) {
-		this.questionRepository = questionRepository;
-	}
-	
-	
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		
-	List<Question> myList = new ArrayList<Question>();
-	String tags[] = {"1","2"};
-		myList = questionRepository.findAll();
-		Question question = myList.get(0);
-		System.out.println(question.getContent());
-		questionRepository.save(new Question("Java", "Java ist cool", tags, new Long(12)));
-	}
-
-}
 	
