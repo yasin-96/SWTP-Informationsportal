9a798640f2b313cae5e0213772a7e1271d82926d
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-11 14:23:24 +0200

    backend: rename the main class that runs the whole application

---
+++ backend/src/main/java/de/thm/swtp/information_portal/InformationPortal.java
@@ -0,0 +1,14 @@
+package de.thm.swtp.information_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationPortal  {
+	public static void main(String[] args) {
+		SpringApplication.run(InformationPortal.class, args);
+	}
+}
+
+
+	
