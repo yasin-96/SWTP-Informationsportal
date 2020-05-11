6aaa137e05329e13c9e28eaecf8e149f829b3f41
Alexander Jajzler <jaijszler@gmail.com>
2020-05-11 11:03:48 +0200

    fronend: start creating card for questions and modify  the designing

---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <v-app-bar
+      color="#7fba25"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="display-1 justify-center"> Informationsportal</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align->
+          <v-tab>Startseite</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+
+    }),
+  }
+</script>
+
+<script>
+  export default {
+    name: 'Navigation',
+    data: () => ({
+
+    }),
+  }
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/QuestionCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container>
+    <v-card
+      class="mx-auto"
+      color="#26c6da"
+      dark
+      max-width="400"
+    >
+      <v-card-title>
+        <v-icon
+          large
+          left
+        >
+          mdi-account
+        </v-icon>
+        <span class="title font-weight-light">
+          <v-list-item-content>
+            <v-list-item-title>User-Name</v-list-item-title>
+          </v-list-item-content>
+        </span>
+      </v-card-title>
+
+      <v-card-text class="headline font-weight-bold">
+        Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+      </v-card-text>
+
+      <v-card-actions>
+        <v-list-item class="grow">
+          <v-row
+            align="center"
+            justify="end"
+          >
+            <v-icon class="mr-1">
+              mdi-heart
+            </v-icon>
+            <span class="subheading mr-2">256</span>
+            <span class="mr-1">·</span>
+          </v-row>
+        </v-list-item>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      //
+    }),
+  }
+</script>
+
+<script>
+export default {
+    name: "QuestionCard",
+    props: {
+		qId: {
+			type: String,
+            default: "",
+            length: 24
+        },
+        qContent: {
+            type: String, 
+            default: ""
+        },
+        qTags: {
+            type: Array,
+            default: Array
+        },
+        qDate: {
+            type: String,
+            default: ""
+        }
+	}
+}
+</script>
+
+<style>
+
+</style>
