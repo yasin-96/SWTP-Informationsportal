4788d148a6cb4952fee290385f5d9e3c1d25dd15
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-11 12:23:31 +0200

    frontend: prepare file that will be used for sending api-calls

---
+++ frontend/src/services/RestCalls.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import http from 'http';
+
+/* Only for develop and debugging */
+let isDebugEnabled = process.env.VUE_APP_API_STATE === 'dev' ? true : false;
+console.log('process.env.VUE_APP_API_STATE: ' + process.env.VUE_APP_API_STATE);
+console.warn('isDebugEnabled: ' + isDebugEnabled);
+
+
+/**
+ * Defines Information about server
+ *
+ * @param {string} apiProtocol - The protocol for the server e.g http/https
+ * @param {string} apiServer - The server ip address /name
+ * @param {string} apiPort - The port interface of the server
+ * @param {string} apiInterface - Interface to send the requests to the server
+ * @param {string} apiUrl - The complete url with the previous parameters
+ * @param {string} apiAddress - The address to the server to answer the requests
+ * @param {string} softwareDevelopState - Gives me the information under which status the server is running. (production or development)
+ */
+const serverConfig = {
+    apiProtocol: process.env.VUE_APP_API_PROTOCOL,
+    apiServer: process.env.VUE_APP_API_SERVER,
+    apiPort: process.env.VUE_APP_API_PORT,
+    apiInterface: process.env.VUE_APP_API_INTERFACE,
+    apiUrl: process.env.VUE_APP_API_URL,
+    apiAddress: process.env.VUE_APP_API_URL,
+    softwareDevelopState: process.env.VUE_APP_API_STATE
+};
+
+/* Set: Axios Instance
+ * These are the properties for axios given with to send requests.
+ */
+const client = new axios.create({
+    baseURL: serverConfig.apiAddress,
+    httpAgent: new http.Agent({ keepAlive: true }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+});
+
+
+/*
+ * Returns the information under which address the backend is to be delivered.
+ * Depending on the start process. These are different under dev and prod.
+ */
+switch (serverConfig.softwareDevelopState) {
+    case 'dev':
+      isDebugEnabled = true;
+      console.log(`REST-API is addressed for DEVELOPMENT at the address: ${serverConfig.apiAddress} .`);
+      break;
+  
+    case 'release':
+      isDebugEnabled = false;
+      console.log(`REST-API is addressed PRODUCTIVELY at the address: ${serverConfig.apiAddress} angesprochen.`);
+      break;
+  
+    default:
+      isDebugEnabled = false;
+      console.error('No <state> selected! Please do not use Frontend productively and contact the responsible developer immediately!');
+      serverConfig.apiAddress = '';
+      break;
+}
+
+
+
+export default {
+
+    async getAllQuestions(){
+        try {
+            let serverResponse = await clien.get(); 
+        } catch (error) {
+            
+        }
+    }
+
+}
