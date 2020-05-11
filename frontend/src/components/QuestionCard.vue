0fef1a6382535d4de8a327cae61d23f32d820e4a
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-12 00:14:15 +0200

    frontend: change template to display props and start rest-call to get data

--- frontend/src/components/QuestionCard.vue
+++ frontend/src/components/QuestionCard.vue
@@ -2,8 +2,8 @@
   <v-container>
     <v-card
       class="mx-auto"
-      color="#26c6da"
-      dark
+      raised
+      shaped
       max-width="400"
     >
       <v-card-title>
@@ -11,67 +11,91 @@
           large
           left
         >
-          mdi-account
+          mdi-account-circle
         </v-icon>
         <span class="title font-weight-light">
           <v-list-item-content>
-            <v-list-item-title>User-Name</v-list-item-title>
+            <v-list-item-title><span class="font-weight-bold">Frage</span> von User</v-list-item-title>
           </v-list-item-content>
         </span>
       </v-card-title>
-
-      <v-card-text class="headline font-weight-bold">
-        Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+      <v-divider></v-divider>
+      <v-card-text class="headline font-weight-bold font-italic">
+        {{ qHeader }}
+      </v-card-text>
+      <v-card-text class="subtitle-1">
+        {{ qContent }}
       </v-card-text>
 
       <v-card-actions>
         <v-list-item class="grow">
           <v-row
             align="center"
+            justify="start"
+          >
+            <v-icon class="mr-1" color="blue">
+              mdi-clock
+            </v-icon>
+            <span class="subheading mr-2"> {{ qDate }}</span>
+          </v-row>
+          <v-row
+            align="center"
             justify="end"
           >
-            <v-icon class="mr-1">
+            <v-icon class="mr-1" color="red">
               mdi-heart
             </v-icon>
             <span class="subheading mr-2">256</span>
-            <span class="mr-1">·</span>
           </v-row>
         </v-list-item>
       </v-card-actions>
+
+      <v-divider></v-divider>
+
+      <v-container v-if="qTags.length">
+        <v-icon color="brown  darken-2" >mdi-label</v-icon>
+          <v-chip v-for="tag in qTags" :key="tag"
+            class="ma-2 caption"
+            label
+            color="brown lighten-2"
+            text-color="white"
+          >
+            {{ tag }}
+          </v-chip>
+      </v-container>
     </v-card>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      //
-    }),
-  }
-</script>
-
-<script>
 export default {
-    name: "QuestionCard",
-    props: {
-		qId: {
-			type: String,
-            default: "",
-            length: 24
-        },
-        qContent: {
-            type: String, 
-            default: ""
-        },
-        qTags: {
-            type: Array,
-            default: Array
-        },
-        qDate: {
-            type: String,
-            default: ""
-        }
-	}
+  name: "QuestionCard",
+  props: {
+    qId: {
+      type: String,
+          default: "",
+          length: 24
+    },
+    qHeader:{
+      type: String,
+      default: ""
+    },
+    qContent: {
+        type: String, 
+        default: ""
+    },
+    qTags: {
+        type: Array,
+        default: Array
+    },
+    qDate: {
+        type: String,
+        default: ""
+    }
+  }, 
+  data: () => ({
+      //
+  }),
 }
 </script>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
  <v-container>
-    <QuestionCard />
+   <v-row>
+     <v-col v-for="quest in allQuestions" :key="quest.id">
+      <QuestionCard 
+        :qId="quest.id"
+        :qHeader="quest.header"
+        :qContent="quest.content"
+        :qTags="quest.tags"
+        :qDate="quest.timeStamp"
+      />
+     </v-col>
+   </v-row>
  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import QuestionCard from "@/components/QuestionCard"
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: "Home",
   components: {
     QuestionCard
-  }
+  },
+  beforeMount: async function() {
+    try {
+      await this.$store.dispatch('act_getAllQuestions');
+    } catch(error) {
+      console.error("beforeMount: ", error);
+    } 
+  },
+  computed: {
+    ...mapActions(['act_getAllQuestions']),
+    ...mapState(['allQuestions']),
+  },
 };
 </script>
