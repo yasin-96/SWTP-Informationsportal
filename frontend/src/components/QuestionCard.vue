f03653f8fe0defa5f6586d8c814f238e711261e8
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-12 11:36:13 +0200

    frontend: UI has been slightly modified and as soon as you click on the title of a question, you will navigate to a new page that reflects exactly this question and shows all answers and comments

--- frontend/src/components/QuestionCard.vue
+++ frontend/src/components/QuestionCard.vue
@@ -3,8 +3,8 @@
     <v-card
       class="mx-auto"
       raised
-      shaped
-      max-width="400"
+      min-width="200"
+      max-height="400"
     >
       <v-card-title>
         <v-icon
@@ -14,36 +14,46 @@
           mdi-account-circle
         </v-icon>
         <span class="title font-weight-light">
-          <v-list-item-content>
+          <v-list-item-content pa-1>
             <v-list-item-title><span class="font-weight-bold">Frage</span> von User</v-list-item-title>
+            <v-list-item-subtitle>
+              
+            <span class="subheading mt-1 mr-2"> 
+              <v-icon color="blue" small>
+                mdi-clock-outline
+              </v-icon>
+              {{ qDate }}
+              </span>
+              </v-list-item-subtitle>
           </v-list-item-content>
         </span>
+        <v-divider inset></v-divider>
       </v-card-title>
-      <v-divider></v-divider>
-      <v-card-text class="headline font-weight-bold font-italic">
-        {{ qHeader }}
+      
+      <v-card-text>
+        <v-content>
+          <p>
+            <label class="headline font-weight-bold font-weight-black font-italic changeMouse" @click="goToQuestion()"> 
+              {{ qHeader }}
+            </label>
+          </p>
+          <p>
+            <label>
+              {{ qContent }}
+            </label>
+          </p>
+        </v-content>
       </v-card-text>
-      <v-card-text class="subtitle-1">
-        {{ qContent }}
-      </v-card-text>
+      
 
       <v-card-actions>
         <v-list-item class="grow">
           <v-row
             align="center"
-            justify="start"
-          >
-            <v-icon class="mr-1" color="blue">
-              mdi-clock
-            </v-icon>
-            <span class="subheading mr-2"> {{ qDate }}</span>
-          </v-row>
-          <v-row
-            align="center"
             justify="end"
           >
-            <v-icon class="mr-1" color="red">
-              mdi-heart
+            <v-icon class="mr-1" color="blue">
+              mdi-thumb-up
             </v-icon>
             <span class="subheading mr-2">256</span>
           </v-row>
@@ -53,15 +63,20 @@
       <v-divider></v-divider>
 
       <v-container v-if="qTags.length">
-        <v-icon color="brown  darken-2" >mdi-label</v-icon>
-          <v-chip v-for="tag in qTags" :key="tag"
-            class="ma-2 caption"
-            label
-            color="brown lighten-2"
-            text-color="white"
+         <v-chip-group
+            multiple
+            show-arrows
+            active-class="primary--text"
           >
-            {{ tag }}
-          </v-chip>
+            <v-chip v-for="tag in qTags" :key="tag" 
+              class="ma-2 caption"
+              label
+              color="brown lighten-2"
+              text-color="white"
+            >
+              {{ tag }}
+            </v-chip>
+          </v-chip-group>
       </v-container>
     </v-card>
   </v-container>
@@ -96,9 +111,16 @@ export default {
   data: () => ({
       //
   }),
+  methods: {
+    goToQuestion() {
+      this.$router.push(`/questionById/${this.$props.qId}`)
+    }
+  }
 }
 </script>
 
 <style>
-
+.changeMouse {
+    cursor: pointer;
+}
 </style>
