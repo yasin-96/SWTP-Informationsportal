a1ebcb48aacec0d02330668581c65fed00b7fb2d
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-14 12:18:05 +0200

    frontend: define color vars for theme

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -7,4 +7,14 @@ export default new Vuetify({
     icons: {
         iconfont: 'mdi',
     },
+    theme: {
+        themes: {
+          light: {
+            primary: '#7fba25',
+            secondary: '#b0bec5',
+            accent: '#8c9eff',
+            danger: '#b71c1c',
+          },
+        },
+      },
 });
