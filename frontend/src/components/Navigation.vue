f332384a7369a960c53859b6c94c5ac8f01a5b02
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-14 12:17:42 +0200

    frontend: use style props

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,16 +1,11 @@
 <template>
   <nav>
-    <v-app-bar
-      color="#7fba25"
-      dark
-      class="overflow-hidden"
-    >
+    <v-app-bar color="primary" dark class="overflow-hidden">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-title class="display-1"> Informationsportal</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
@@ -22,31 +17,27 @@
             <v-icon>
               mdi-home
             </v-icon>
-              Startseite
+            Startseite
           </v-tab>
-          <v-tab @click="goToLink('notification')">
-            Mitteilungen</v-tab>
+          <v-tab @click="goToLink('notification')"> Mitteilungen</v-tab>
           <v-spacer></v-spacer>
         </v-tabs>
-      </template> 
+      </template>
     </v-app-bar>
   </nav>
 </template>
 
 <script>
-  export default {
-    name: 'Navigation',
-    data: () => ({
+export default {
+  name: 'Navigation',
+  data: () => ({
 
-    }),
-    methods: {
-      goToLink(link){
-        this.$router.push(`/${link}`);
-      }
+  }),
+  methods: {
+    goToLink(link){
+      this.$router.push(`/${link}`);
     }
   }
+}
 </script>
-
-<style>
-
-</style>
+<style></style>
