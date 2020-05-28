f7c86a2fd90699be57dbb4efe905c36716ee82e3
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-28 11:28:22 +0200

    frontend: add new component for answers and comments

--- frontend/src/components/NewContent.vue
+++ frontend/src/components/NewContent.vue
@@ -1,13 +1,17 @@
 <template>
   <b-container class="pt-4 pb-4">
     <b-button-group class="d-flex">
-      <b-form-textarea :placeholder="nPlaceholder" :rows="nRows" :no-resize="nResize" :size="bTextSize"></b-form-textarea>
-      <b-button>{{ bText }}</b-button>
+      <b-form-textarea v-if="nIsAnswer" v-model="contentForAnswer" placeholder="Add new answer ..." :rows="nRows" :no-resize="nResize" :size="bTextSize"></b-form-textarea>
+      <b-form-textarea v-if="nIsComment" v-model="contentForComment" placeholder="Add new comment ..." :rows="nRows" :no-resize="nResize" :size="bTextSize"></b-form-textarea>
+      <b-button v-if="nIsAnswer" @click="addNewAnswer()">{{ bText }}</b-button>
+      <b-button v-if="nIsComment" @click="addNewComment()">{{ bText }}</b-button>
     </b-button-group>
   </b-container>
 </template>
 
 <script>
+
+import {mapActions} from 'vuex'
 export default {
   name: 'NewContent',
   props: {
@@ -30,8 +34,67 @@ export default {
     bTextSize: {
         type: String,
         default: 'sm'
+    },
+    nIsAnswer: {
+        type: Boolean,
+        default: false,
+    },
+    nIsComment: {
+        type: Boolean,
+        default: false,
+    },
+    nId: {
+      type: String,
+      default: "",
     }
   },
+  mounted() {
+    console.warn("ROUNTER -> ", this.$router.history.current.params.id);
+    this.newAnswer.id = this.$router.history.current.params.id;
+    this.newComment.id = this.$router.history.current.params.id;
+  },
+  data: () => ({
+    newAnswer: {
+      id: '',
+      listOfAnswers: []
+    },
+    newComment: {
+      id: '',
+      comments: []
+    },
+    contentForAnswer: "",
+    contentForComment: ""
+  }),
+  computed: {
+    ...mapActions(['act_addNewAnswer', 'act_addNewComment'])
+  },
+  methods: {
+    async addNewAnswer(){
+      if(this.contentForAnswer) {
+        this.newAnswer.listOfAnswers.push({
+          content: this.contentForAnswer,
+          rating: 0,
+          timeStamp: Date.parse(new Date()),
+        });
+        console.log("HIER:!!", this.newAnswer);
+        let resp = await this.$store.dispatch('act_addNewAnswer', this.newAnswer);
+        this.$router.go(`/question/${resp.id}`)
+      }
+    },
+
+    async addNewComment(){
+      if(this.contentForComment) {
+        this.newComment.comments.push({
+          content: this.contentForComment,
+          rating: 0,
+          timestamp: Date.parse(new Date()),
+        });
+        console.log("HIER:!!", this.newComment);
+        let resp = await this.$store.dispatch('act_addNewComment', this.newComment);
+        this.$router.go(`/question/${resp.id}`)
+      }
+    }
+  }
 };
 </script>
 
