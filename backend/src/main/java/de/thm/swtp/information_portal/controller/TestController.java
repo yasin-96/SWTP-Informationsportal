e9956dc6730c37a38e2d622833ecfc514fd5b8f5
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-30 19:28:55 +0200

    backend: test controller

---
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/TestController.java
@@ -0,0 +1,44 @@
+package de.thm.swtp.information_portal.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.validator.internal.util.logging.Log_.logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.thm.swtp.information_portal.models.Answer;
+import de.thm.swtp.information_portal.models.Answers;
+import de.thm.swtp.information_portal.models.Comment;
+import de.thm.swtp.information_portal.models.Comments;
+import de.thm.swtp.information_portal.repositories.CommentRepository;
+import de.thm.swtp.information_portal.service.CommentService;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String newMessage(@AuthenticationPrincipal Jwt jwt) {
+        logger.info(jwt.getClaim("name"));
+
+        logger.info("New Message -> in");
+
+        rabbitTemplate.convertAndSend("messages", new Message("New Message -> in"));
+
+        return "New Message";
+    }
+}
