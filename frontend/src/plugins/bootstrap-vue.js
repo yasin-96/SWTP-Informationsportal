71c52d9dd9d757add73e43590cb5d22d54e0e6a5
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-05-31 20:08:33 +0200

    frontend: format settings

--- frontend/src/plugins/bootstrap-vue.js
+++ frontend/src/plugins/bootstrap-vue.js
@@ -4,6 +4,5 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
 Vue.use(BootstrapVueIcons)
 Vue.use(BootstrapVue)
