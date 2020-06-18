74c2a5f003e93ea34ec20754013ab22bce42950d
Alexander Jajzler <alexander.jajzler@sbdinc.com>
2020-06-18 14:15:04 +0200

    frontend: add ui for new api calls, this will display most active question as well questions by topics

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,51 @@
-<template></template>
+<template>
+ <b-container v-if="questions.length">
+   <b-row>
+     <b-col sm="12" md="6" lg="4" xl="3" v-for="quest in questions" :key="quest.id" class="mt-4">
+      <QuestionCard 
+        :qId="quest.id"
+        :qHeader="quest.header"
+        :qContent="quest.content"
+        :qTags="quest.tags"
+        :qDate="quest.timeStamp"
+        :qTrimText="true"
+      />
+     </b-col>
+   </b-row>
+ </b-container>
+</template>
 
 <script>
-import CreateNewQuestion from "@/components/CreateNewQuestion";
+// @ is an alias to /src
+import QuestionCard from "@/components/QuestionCard"
+import { mapState, mapActions } from 'vuex';
 
 export default {
+  name: "Home",
   components: {
-    CreateNewQuestion
+    QuestionCard
   },
-  methods: {
-    goToLink(link){
-      this.$router.push(`/${link}`);
+  async beforeMount() {
+    try {
+      await this.$store.dispatch('act_getMostActiveQuestions');
+    } catch(error) {
+      console.error("beforeMount: ", error);
+    } 
+  },
+  computed: {
+    ...mapActions(['act_getMostActiveQuestions',]),
+    ...mapState(['activeQuestions']),
+    
+    questions(){
+      if(!!this.activeQuestions){
+        return this.activeQuestions;
+      }
     }
+  },
+  watch: { 
+    activeQuestions(){
+      this.$forceUpdate();
+    },
   }
-}
+};
 </script>
-
-<style>
-</style>
