494ff0f55d7f168f7a6f942e4dc11ebf3bc442aa
yers32 <yasin.eraslan@mni.thm.de>
2020-07-01 21:26:52 +0200

    changed endpoint from GET to POST

--- backend/src/main/java/de/thm/swtp/information_portal/controller/UserController.java
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
     }
 
     @Async
-    @GetMapping("/nameFromId")
+    @PostMapping("/nameFromId")
     CompletableFuture<ResponseEntity<String>> getNameFromId(@RequestBody String id){
         Optional<User> user = userService.getUser(id);
         String name = user.get().getName();
