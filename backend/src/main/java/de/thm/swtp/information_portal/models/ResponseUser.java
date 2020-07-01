0cfb7c686d1d3dca5d077a4ff436581b764ebdcb
yers32 <yasin.eraslan@mni.thm.de>
2020-07-01 23:29:51 +0200

    bugfix

--- backend/src/main/java/de/thm/swtp/information_portal/models/ResponseUser.java
+++ backend/src/main/java/de/thm/swtp/information_portal/models/ResponseUser.java
@@ -1,5 +1,10 @@
 package de.thm.swtp.information_portal.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ResponseUser {
     String name;
 
