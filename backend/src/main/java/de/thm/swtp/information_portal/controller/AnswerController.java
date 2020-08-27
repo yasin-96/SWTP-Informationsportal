473dbbbc21f637cfc666f6b6d5ff5b5d0d60388c
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2020-08-27 15:35:30 +0200

    move logic from answer controller to service

--- backend/src/main/java/de/thm/swtp/information_portal/controller/AnswerController.java
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/AnswerController.java
@@ -24,152 +24,72 @@ import de.thm.swtp.information_portal.service.UserService;
 @CrossOrigin(origins = "*")
 public class AnswerController {
 
-	@Autowired
-	private AnswerService answerService;
+    @Autowired
+    private AnswerService answerService;
 
-	@Autowired
-	private UserService userService;
+    /**
+     * @param answerList
+     * @return
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+    @Async
+    @PostMapping("/answer")
+    public CompletableFuture<ResponseEntity<Answers>> postAnswer(@RequestBody Answers answerList,
+                                                                 @AuthenticationPrincipal Jwt jwt) throws URISyntaxException {
+        //TODO anserlist prüfen
+        var jwtSub = jwt.getClaimAsString("sub");
+        return CompletableFuture.completedFuture(answerService.add(answerList, jwtSub));
+    }
 
-	/**
-	 * 
-	 * @param answerList
-	 * @return
-	 * @throws URISyntaxException
-	 * @throws InterruptedException
-	 */
-	@Async
-	@PostMapping("/answer")
-	public CompletableFuture<ResponseEntity<Answers>> postAnswer(@RequestBody Answers answerList,
-			@AuthenticationPrincipal Jwt jwt) throws URISyntaxException, InterruptedException {
-		Optional<Answers> answers = answerService.findByQuestionId(answerList.getId());
-		if (answers.isEmpty()) {
-			List<Answer> newAnswerList = new ArrayList<Answer>();
-			Answer newAnswer = new Answer(answerList.getListOfAnswers().get(0).getContent(),
-					jwt.getClaimAsString("sub"), 0);
-			newAnswerList.add(newAnswer);
-			Answers newAnswers = new Answers(newAnswerList, answerList.getId());
-			answerService.postAnswer(newAnswers);
-			return CompletableFuture.completedFuture(
-					ResponseEntity.created(new URI("/api/answer" + newAnswers.getId())).body(newAnswers));
-		} else {
-			List<Answer> answersPresent = answers.get().getListOfAnswers();
-			Answer newAnswer = new Answer(answerList.getListOfAnswers().get(0).getContent(),
-					jwt.getClaimAsString("sub"), 0);
-			answersPresent.add(newAnswer);
-			answerService.postAnswer(answers.get());
-			return CompletableFuture.completedFuture(
-					ResponseEntity.created(new URI("/api/answer" + answers.get().getId())).body(answers.get()));
-		}
-	}
+    /**
+     * @param ids
+     * @return
+     */
+    @Async
+    @PostMapping("/answer/answerTobeEdited")
+    public CompletableFuture<ResponseEntity<Answer>> getAnswerToBeEdited(@Validated @RequestBody String[] ids) {
 
-	/**
-	 *
-	 * @param ids
-	 * @return
-	 */
-	@Async
-	@PostMapping("/answer/answerTobeEdited")
-	public CompletableFuture<ResponseEntity<Answer>> getAnswerToBeEdited(@Validated @RequestBody String[] ids) {
-		Optional<Answers> answers = answerService.findByQuestionId(ids[0]);
-		List<Answer> answerList = answers.get().getListOfAnswers();
-		var foundAnswer = new Answer();
-		for (var answer : answerList) {
-			if (answer.getId().equals(ids[1])) {
-				foundAnswer = answer;
+        //TODO: IDS prüfen
 
-			}
-		}
-		return CompletableFuture.completedFuture(new ResponseEntity<Answer>(foundAnswer, HttpStatus.OK));
-	}
+        return CompletableFuture.completedFuture(answerService.getAnswerToEdit(ids));
+    }
 
-	/**
-	 *
-	 * @param answersBody
-	 * @return
-	 * @throws URISyntaxException
-	 */
-	@Async
-	@PutMapping("/answer")
-	public CompletableFuture<ResponseEntity<Answers>> editAnswer(@Validated @RequestBody Answers answersBody)
-			throws URISyntaxException {
-		Optional<Answers> answersToBeModified = answerService.findByQuestionId(answersBody.getId());
-		Answer modifiedAnswer = answersBody.getListOfAnswers().get(0);
-		List<Answer> listOfAnswers = answersToBeModified.get().getListOfAnswers();
-		listOfAnswers.forEach((item -> {
-			if (item.getId().equals(modifiedAnswer.getId())) {
-				int index = listOfAnswers.indexOf(item);
-				listOfAnswers.set(index, modifiedAnswer);
-			}
-		}));
-		answersToBeModified.get().setListOfAnswers(listOfAnswers);
-		answerService.postAnswer(answersToBeModified.get());
-		return CompletableFuture.completedFuture(ResponseEntity
-				.created(new URI("/api/answer/" + answersToBeModified.get().getId())).body(answersToBeModified.get()));
-	}
+    /**
+     * @param answersBody
+     * @return
+     * @throws URISyntaxException
+     */
+    @Async
+    @PutMapping("/answer")
+    public CompletableFuture<ResponseEntity<Answers>> editAnswer(@Validated @RequestBody Answers answersBody) throws URISyntaxException {
+        //TODO objekt prüfen??
+        return CompletableFuture.completedFuture(answerService.updateAnwer(answersBody));
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return
-	 * @throws InterruptedException
-	 * @throws InterruptedException
-	 */
-	@Async
-	@GetMapping("/answersByQuestionId/{id}")
-	public CompletableFuture<ResponseEntity<Answers>> getAnswers(@PathVariable String id)
-			throws InterruptedException, InterruptedException {
-		Optional<Answers> answers = answerService.findByQuestionId(id);
-		if (answers.isPresent()) {
-			List<Answer> allAnswers = answers.get().getListOfAnswers();
+    /**
+     * @param id
+     * @return
+     * @throws InterruptedException
+     * @throws InterruptedException
+     */
+    @Async
+    @GetMapping("/answersByQuestionId/{id}")
+    public CompletableFuture<ResponseEntity<Answers>> getAnswers(@PathVariable String id) {
 
-			//write userName to structure
-			allAnswers.forEach(aData -> {
+        //TODO ID prüfen?
+        return CompletableFuture.completedFuture(answerService.getAnswers(id));
+    }
 
-				if (aData.getUserId() != null) {
-					var userIdFromAnswer = aData.getUserId();
-					var userName = userService.getUser(userIdFromAnswer).get().getPreferred_username();
-					aData.setUserName( !userName.isEmpty() && userName != null ? userName : "Unknown");
-				} else {
-					aData.setUserName("Unknown");
-				}
+    /**
+     * @param answerList
+     * @return
+     */
+    @Async
+    @PostMapping("/answer/increaseRating")
+    public CompletableFuture<ResponseEntity<Answers>> increaseAnswerRating(@RequestBody Answers answerList) {
 
-			});
-
-			allAnswers.sort(compareByRating);
-		}
-		ResponseEntity<Answers> answRes = answers.map(response -> ResponseEntity.ok().body(response))
-				.orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
-		return CompletableFuture.completedFuture(answRes);
-	}
-
-	Comparator<Answer> compareByRating = new Comparator<Answer>() {
-		@Override
-		public int compare(Answer o1, Answer o2) {
-			return o2.getRating() - o1.getRating();
-		}
-	};
-
-	/**
-	 *
-	 * @param answerList
-	 * @return
-	 */
-	@Async
-	@PostMapping("/answer/increaseRating")
-	public CompletableFuture<ResponseEntity<Answers>> increaseAnswerRating(@RequestBody Answers answerList) {
-		Optional<Answers> answersToBeModified = answerService.findByQuestionId(answerList.getId());
-		Answer modifiedAnswer = answerList.getListOfAnswers().get(0);
-		List<Answer> listOfAnswers = answersToBeModified.get().getListOfAnswers();
-		listOfAnswers.forEach((item -> {
-			if (item.getId().equals(modifiedAnswer.getId())) {
-				int index = listOfAnswers.indexOf(item);
-				listOfAnswers.set(index, modifiedAnswer);
-			}
-		}));
-		answersToBeModified.get().setListOfAnswers(listOfAnswers);
-		ResponseEntity<Answers> answRes = answersToBeModified.map(response -> ResponseEntity.ok().body(response))
-				.orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
-		answerService.postAnswer(answersToBeModified.get());
-		return CompletableFuture.completedFuture(answRes);
-	}
+        //CHECK answerlist
+        return CompletableFuture.completedFuture(answerService.increaseRating(answerList));
+    }
 }
