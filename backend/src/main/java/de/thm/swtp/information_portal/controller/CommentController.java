7cd4b6419d6e34794cd0cbd89c0a97d4f48adde0
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2020-08-28 10:08:01 +0200

    move logic from comment controller into service

--- backend/src/main/java/de/thm/swtp/information_portal/controller/CommentController.java
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/CommentController.java
@@ -33,126 +33,59 @@ import de.thm.swtp.information_portal.service.UserService;
 @CrossOrigin(origins = "*")
 public class CommentController {
 
-	@Autowired
-	private CommentService commentService;
+    @Autowired
+    private CommentService commentService;
 
-	
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	/**
-	 * 
-	 * @return
-	 * @throws InterruptedException
-	 */
-	@Async
-	@GetMapping("/allComments")
-	public CompletableFuture<List<Comments>> findAllComments() throws InterruptedException {
-		return CompletableFuture.completedFuture(commentService.findAllComments());
-	}
+    /**
+     * @return
+     * @throws InterruptedException
+     */
+    @Async
+    @GetMapping("/allComments")
+    public CompletableFuture<List<Comments>> findAllComments() throws InterruptedException {
+        return CompletableFuture.completedFuture(commentService.findAllComments());
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return
-	 * @throws InterruptedException
-	 */
-	@Async
-	@GetMapping("/commentsByAnswerId/{id}")
-	public CompletableFuture<ResponseEntity<Comments>> findByAnswerId(@PathVariable String id)
-			throws InterruptedException {
-		Optional<Comments> comments = commentService.findByAnswerId(id);
-		if (comments.isPresent()) {
-			List<Comment> allComments = comments.get().getComments();
+    /**
+     * @param id
+     * @return
+     * @throws InterruptedException
+     */
+    @Async
+    @GetMapping("/commentsByAnswerId/{id}")
+    public CompletableFuture<ResponseEntity<Comments>> findByAnswerId(@PathVariable String id)
+            throws InterruptedException {
+        //TODO id prüfen oder Springboot es überlassen?!
+        return CompletableFuture.completedFuture(commentService.getCommentsByAnswerId(id));
+    }
 
-			allComments.forEach(comment -> {
+    /**
+     * @param commentList
+     * @return
+     */
+    @Async
+    @PostMapping("/comment/increaseRating")
+    public CompletableFuture<ResponseEntity<Comments>> increaseCommentRating(
+            @Validated @RequestBody Comments commentList) {
+        //TODO liste validieren?
+        return CompletableFuture.completedFuture(commentService.increaseRating(commentList));
+    }
 
-				if(comment.getUserId() != null) {
-
-					var userName = userService.getUser(comment.getUserId()).get().getPreferred_username();
-
-					comment.setUserName(
-						!userName.isEmpty() && userName != null ? userName : "Unknown");
-				} else {
-					comment.setUserName("Unknown");
-				}
-			});
-
-
-
-
-			allComments.sort(compareByRating);
-		}
-		ResponseEntity<Comments> resComments = comments.map(response -> ResponseEntity.ok().body(response))
-				.orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
-		return CompletableFuture.completedFuture(resComments);
-	}
-
-	Comparator<Comment> compareByRating = new Comparator<Comment>() {
-		@Override
-		public int compare(Comment o1, Comment o2) {
-			return o2.getRating() - o1.getRating();
-		}
-	};
-
-	/**
-	 *
-	 * @param commentList
-	 * @return
-	 */
-	@Async
-	@PostMapping("/comment/increaseRating")
-	public CompletableFuture<ResponseEntity<Comments>> increaseCommentRating(
-			@Validated @RequestBody Comments commentList) {
-		Optional<Comments> commentsToBeModified = commentService.findByAnswerId(commentList.getId());
-		Comment modifiedComment = commentList.getComments().get(0);
-		List<Comment> listOfComments = commentsToBeModified.get().getComments();
-		listOfComments.forEach((item -> {
-			if (item.getId().equals(modifiedComment.getId())) {
-				int index = listOfComments.indexOf(item);
-				listOfComments.set(index, modifiedComment);
-			}
-		}));
-		commentsToBeModified.get().setComments(listOfComments);
-		ResponseEntity<Comments> comRes = commentsToBeModified.map(response -> ResponseEntity.ok().body(response))
-				.orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
-		commentService.postComments(commentsToBeModified.get());
-		return CompletableFuture.completedFuture(comRes);
-	}
-
-	/**
-	 * 
-	 * @param commentList
-	 * @return
-	 * @throws URISyntaxException
-	 * @throws InterruptedException
-	 */
-	@Async
-	@PostMapping("/newComments")
-	public CompletableFuture<ResponseEntity<Comments>> postComments(@RequestBody Comments commentList, @AuthenticationPrincipal Jwt jwt)
-			throws URISyntaxException, InterruptedException {
-		Optional<Comments> comments = commentService.findByAnswerId(commentList.getId());
-		Comment existingComment = commentList.getComments().get(0);
-		if (!comments.isPresent()) {
-			List<Comment> newCommentList = new ArrayList<Comment>();
-			Comment newComment = new Comment(existingComment.getContent(), jwt.getClaimAsString("sub"),
-					existingComment.getRating());
-			newCommentList.add(newComment);
-			Comments newComments = new Comments(newCommentList, commentList.getId());
-			commentService.postComments(newComments);
-			return CompletableFuture.completedFuture(
-					ResponseEntity.created(new URI("/api/answer" + newComments.getId())).body(newComments));
-		}
-
-		else {
-			List<Comment> commentsPresent = comments.get().getComments();
-			Comment newComment = new Comment(existingComment.getContent(), jwt.getClaimAsString("sub"),
-					existingComment.getRating());
-			commentsPresent.add(newComment);
-			comments.get().setComments(commentsPresent);
-			commentService.postComments(comments.get());
-			return CompletableFuture.completedFuture(
-					ResponseEntity.created(new URI("/api/answer" + comments.get().getId())).body(comments.get()));
-		}
-	}
+    /**
+     * @param commentList
+     * @return
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+    @Async
+    @PostMapping("/newComments")
+    public CompletableFuture<ResponseEntity<Comments>> postComments(@RequestBody Comments commentList, @AuthenticationPrincipal Jwt jwt)
+            throws URISyntaxException {
+        //TODO liste prüfen?
+        var jwtSub = jwt.getClaimAsString("sub");
+        return CompletableFuture.completedFuture(commentService.add(commentList, jwtSub));
+    }
 }
--- backend/src/main/java/de/thm/swtp/information_portal/service/CommentService.java
+++ backend/src/main/java/de/thm/swtp/information_portal/service/CommentService.java
@@ -1,9 +1,19 @@
 package de.thm.swtp.information_portal.service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
+import de.thm.swtp.information_portal.models.Answers;
+import de.thm.swtp.information_portal.models.Comment;
+import de.thm.swtp.information_portal.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import de.thm.swtp.information_portal.models.Comments;
@@ -12,32 +22,145 @@ import de.thm.swtp.information_portal.repositories.CommentRepository;
 @Service
 public class CommentService {
 
-	@Autowired
-	private CommentRepository commentRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-	/**
-	 *
-	 * @return
-	 */
-	public List<Comments>findAllComments(){
-		return commentRepository.findAll();
-	}
+    @Autowired
+    private UserRepository userRepository;
 
-	/**
-	 *
-	 * @param id
-	 * @return
-	 */
-	public Optional<Comments> findByAnswerId(String id) {
-		return commentRepository.findById(id);
-	}
+    /**
+     * @return
+     */
+    public List<Comments> findAllComments() {
+        return commentRepository.findAll();
+    }
 
-	/**
-	 *
-	 * @param comments
-	 * @return
-	 */
-	public Comments postComments(Comments comments) {
-		return commentRepository.save(comments);
-	}
+    /**
+     * @param id
+     * @return
+     */
+    public Optional<Comments> findByAnswerId(String id) {
+        return commentRepository.findById(id);
+    }
+
+    /**
+     * @param comments
+     * @return
+     */
+    public Comments postComments(Comments comments) {
+        return commentRepository.save(comments);
+    }
+
+    /**
+     *
+     * @param commentList
+     * @param jwtSub
+     * @return
+     * @throws URISyntaxException
+     */
+    public ResponseEntity<Comments> add(Comments commentList, String jwtSub) throws URISyntaxException {
+        var comments = this.findByAnswerId(commentList.getId());
+        var existingComment = commentList.getComments().get(0);
+        if (!comments.isPresent()) {
+            var newComments = new Comments(
+                    List.of(
+                            new Comment(
+                                    commentList.getComments().get(0).getContent(),
+                                    jwtSub,
+                                    commentList.getComments().get(0).getRating()
+                            )
+                    ),
+                    commentList.getId()
+            );
+            this.postComments(newComments);
+
+            return ResponseEntity.created(new URI("/api/answer" + newComments.getId())).body(newComments);
+        } else {
+            var commentsPresent = comments.get().getComments();
+            var newComment = new Comment(
+                    existingComment.getContent(),
+                    jwtSub,
+                    existingComment.getRating()
+            );
+            //TODO add und set ? brauchen wir hier beides
+            commentsPresent.add(newComment);
+            comments.get().setComments(commentsPresent);
+
+            this.postComments(comments.get());
+            return ResponseEntity.created(new URI("/api/answer" + comments.get().getId())).body(comments.get());
+        }
+    }
+
+
+    /**
+     *
+     * @param answerId
+     * @return
+     */
+    public ResponseEntity<Comments> getCommentsByAnswerId(String answerId) {
+        var comments = this.findByAnswerId(answerId);
+        if (comments.isPresent()) {
+            var allComments = comments.get().getComments();
+
+            allComments.forEach(comment -> {
+                if (comment.getUserId() != null) {
+                    var user = userRepository.findById(comment.getUserId());
+                    if (user.isPresent()) {
+                        var userName = user.get().getPreferred_username();
+                        comment.setUserName(
+                                !userName.isEmpty() ? userName : "Unknown");
+                    }
+                } else {
+                    comment.setUserName("Unknown");
+                }
+            });
+
+            //TODO sortiert aber nicht benutzt?
+            allComments.sort(compareByRating);
+        }
+        //TODO auch hier wird comments zurück gegeben und nicht das sortiert ?
+        return comments
+                .map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
+    /**
+     *
+     * @param commentList
+     * @return
+     */
+    public ResponseEntity<Comments> increaseRating(Comments commentList) {
+        var commentsToBeModified = this.findByAnswerId(commentList.getId());
+        var modifiedComment = commentList.getComments().get(0);
+
+        var listOfComments = commentsToBeModified.get().getComments();
+
+        listOfComments.forEach((item -> {
+            if (item.getId().equals(modifiedComment.getId())) {
+                listOfComments.set(listOfComments.indexOf(item), modifiedComment);
+            }
+        }));
+
+        //change data in list
+        commentsToBeModified.get().setComments(listOfComments);
+
+        //TODO PRÜFEN ob geklappt hat?
+        this.postComments(commentsToBeModified.get());
+
+        return commentsToBeModified
+                .map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
+
+    /**
+     *
+     */
+    Comparator<Comment> compareByRating = new Comparator<Comment>() {
+        @Override
+        public int compare(Comment o1, Comment o2) {
+            return o2.getRating() - o1.getRating();
+        }
+    };
+
 }
