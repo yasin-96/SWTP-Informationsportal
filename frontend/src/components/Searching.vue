4a9d937a9a3f3e2f4c77bc2022eea92562693035
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2020-08-30 20:05:30 +0200

    add keydown event

--- frontend/src/components/Searching.vue
+++ frontend/src/components/Searching.vue
@@ -3,12 +3,16 @@
     <b-row>
       <b-col>
         <b-input-group class="roundedCornerLeft">
-          <b-input-group-append > 
+          <b-input-group-append>
             <b-button variant="success" v-on:enter="searchData()" @click="searchData()">
               <fai icon="search" />
             </b-button>
           </b-input-group-append>
-          <b-form-input @click="searchData()" v-model="searchInput" placeholder="Search by tags"></b-form-input>
+          <b-form-input
+            @keydown.enter="searchData()"
+            v-model="searchInput"
+            placeholder="Search by tags"
+          ></b-form-input>
         </b-input-group>
       </b-col>
     </b-row>
@@ -33,14 +37,16 @@ export default {
      * The search query is sent to the backend to get a result
      */
     searchData() {
-      this.$router
-        .push({
-          path: '/search',
-          query: {
-            q: `${String(this.searchInput).toUpperCase()}`,
-          },
-        })
-        .catch((err) => {});
+      if (this.searchInput.length > 0) {
+        this.$router
+          .push({
+            path: '/search',
+            query: {
+              q: `${String(this.searchInput).toUpperCase()}`,
+            },
+          })
+          .catch((err) => {});
+      }
     },
   },
   computed: { ...mapActions(['act_getAllDataByQuery']) },
