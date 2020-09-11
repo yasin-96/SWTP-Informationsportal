b9ce0679b07ad10fd0544a67a62dee0b1dc313a5
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2020-09-11 09:20:03 +0200

    change index controller

--- backend/src/main/java/de/thm/swtp/information_portal/controller/IndexController.java
+++ backend/src/main/java/de/thm/swtp/information_portal/controller/IndexController.java
@@ -4,14 +4,23 @@ import com.google.common.net.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    public ResponseEntity<Void> index() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, "index")
+                .build();
+    }
+
+    /*@RequestMapping("/")
     @ResponseBody
     public String index() {
         return "index";
-    }
+    }*/
+
 }
