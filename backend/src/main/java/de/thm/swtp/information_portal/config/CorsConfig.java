a18dbfa557da7c22b57b101594332faa2ca72164
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2020-09-12 13:15:30 +0200

    update

--- backend/src/main/java/de/thm/swtp/information_portal/config/CorsConfig.java
+++ backend/src/main/java/de/thm/swtp/information_portal/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry
         .addMapping("/**")
-        .allowedOrigins("*")
+        .allowedOrigins("http://localhost:8001", "https://swtp.pcvolkmer.de")
         .allowedHeaders("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH");
     }
